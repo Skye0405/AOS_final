25a1d7062380efcef42f598076c43642c4b2e6ca
Kryster39 <ysl2@hotmail.com.tw>
2022-12-26 23:46:54 +0800

    UI: 我有話要說(申訴) 90%

--- app/src/main/java/com/example/aos/Appeal.java
+++ app/src/main/java/com/example/aos/Appeal.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Appeal extends AppCompatActivity {
     TextView appeal;
-    Button cancel;
+    EditText location, time, bus_number, driver, user, contact, subject, content;
+    Button cancel, send;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -19,7 +22,34 @@ public class Appeal extends AppCompatActivity {
         setContentView(R.layout.activity_appeal);
         appeal = findViewById(R.id.mail);
         cancel = findViewById(R.id.cancel);
+        location = findViewById(R.id.location);
+        time = findViewById(R.id.time);
+        bus_number = findViewById(R.id.bus_number);
+        driver = findViewById(R.id.driver);
+        user = findViewById(R.id.user_name);
+        contact = findViewById(R.id.contact);
+        subject = findViewById(R.id.subject);
+        content = findViewById(R.id.content);
+        send = findViewById(R.id.send);
 
         cancel.setOnClickListener(view -> startActivity(new Intent(this, MainActivity.class)));
+
+        send.setOnClickListener(view -> {
+            String s_location = location.getText().toString();
+            String s_time = time.getText().toString();
+            String s_bus_number = bus_number.getText().toString();
+            String s_driver = driver.getText().toString();
+            String s_user = user.getText().toString();
+            String s_contact = contact.getText().toString();
+            String s_subject = subject.getText().toString();
+            String s_content = content.getText().toString();
+
+            if (s_location.isEmpty())
+                System.out.println("Empty");
+            else
+                System.out.println("Non empty");
+
+            //判斷必填資料 送資料庫
+        });
     }
 }
